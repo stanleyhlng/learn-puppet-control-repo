3dfa73d846397d2a1edce242b03917210b0b442b
Stanley Ng <stanleyhlng@users.noreply.github.com>
2021-06-04 11:08:30 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure  => present,
     user    => 'root',
     type    => 'ssh-rsa',
-    key     => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL3UydWBTVwd2/d8DaDMQW1BcMLOmiDPCf5lc9DrLqZTF6aHR5tv3f0VHpBYtMz/csyVprkhezfggX49oUK0BtiIIsOHoIlANucswOoiK6BSBlo90psvTO1iGyTuUML/fWQHPtnuX8Q4Tj7ZligcxzxNTyj84AFIDr6o7vPB/r7hOArzckWxPzCfvdMlwEWqnJPdA4u55lAnaG3CHFaC5S4lS8+nG4EPwBohD13i79wcIym+wonjiJs8ZBYLhJRZkqjsYjDG4bmHLifzHuGm7U6NHXZ/vuZDtxSjVGRQ1ifMTIWUmUb6z3bn2MkiFc1Q0iDiT8jEF7nhz1mR/UqLw3',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDL3UydWBTVwd2/d8DaDMQW1BcMLOmiDPCf5lc9DrLqZTF6aHR5tv3f0VHpBYtMz/csyVprkhezfggX49oUK0BtiIIsOHoIlANucswOoiK6BSBlo90psvTO1iGyTuUML/fWQHPtnuX8Q4Tj7ZligcxzxNTyj84AFIDr6o7vPB/r7hOArzckWxPzCfvdMlwEWqnJPdA4u55lAnaG3CHFaC5S4lS8+nG4EPwBohD13i79wcIym+wonjiJs8ZBYLhJRZkqjsYjDG4bmHLifzHuGm7U6NHXZ/vuZDtxSjVGRQ1ifMTIWUmUb6z3bn2MkiFc1Q0iDiT8jEF7nhz1mR/UqLw3',
   }
 }

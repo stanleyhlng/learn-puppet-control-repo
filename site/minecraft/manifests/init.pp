cfb6045ee1fbfbf7335ba83ae7d958d9ccd69cdd
Stanley Ng <stanleyhlng@users.noreply.github.com>
2021-06-04 13:06:46 -0700

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,16 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/mincraft/minecraft_server.jar':
+    ensure => file,
+    source => 'http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}

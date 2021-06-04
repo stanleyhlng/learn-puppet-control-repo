8c4ad448c59ffac66cb0192bd963de8bf6422d9d
Stanley Ng <stanleyhlng@users.noreply.github.com>
2021-06-04 10:58:16 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,8 +3,8 @@ node default {
 
 node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/README',
-    ensure => true,
+  file { '/root/README':
+    ensure => file,
     content => $fqdn,
   }
 }
